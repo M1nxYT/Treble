17976173c1a2d998c55225ae2b39e4b64ec2c007
MinxterYT <minxter.yt@gmail.com>
2021-10-02 00:41:15 +0100

    until i fix ping command it will operate like a normal command as to prevent the interaction timeout problem

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -7,9 +7,8 @@ export let ping = {
     description: 'Ping Pong!',
     data: new SlashCommandBuilder().setName('ping').setDescription('Check the bot\'s ping'),
     ephemeral: false,
-    deferReply: false,
     
     async execute(interaction: CommandInteraction, client: customClient) {
-        interaction.reply({content: `Current Ping: ${Math.round(client.ws.ping)}ms`})
+        interaction.followUp({content: `Current Ping: ${Math.round(client.ws.ping)}ms`})
     }
 }
